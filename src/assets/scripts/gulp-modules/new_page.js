27e9a8e8d931a531ac42488d04463eeac16d760d
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-11-18 15:44:45 +0200

    feat: add animation for all section and optimize images

--- src/assets/scripts/gulp-modules/new_page.js
+++ src/assets/scripts/gulp-modules/new_page.js
@@ -7,8 +7,14 @@ import { initSmoothScrolling } from '../modules/scroll/leniscroll';
 
 initSmoothScrolling();
 gsap.registerPlugin(ScrollTrigger, CustomEase);
+
 document.addEventListener('DOMContentLoaded', () => {
-  const gallerySwiper = new Swiper('.gallery-swiper', {
+  initGallerySwiper();
+  initAnimations();
+});
+
+function initGallerySwiper() {
+  new Swiper('.gallery-swiper', {
     modules: [Navigation],
     slidesPerView: 1,
     spaceBetween: 10,
@@ -17,9 +23,44 @@ document.addEventListener('DOMContentLoaded', () => {
       prevEl: '.gallery-prev-btn',
     },
   });
-});
+}
 
-document.addEventListener('DOMContentLoaded', function() {
+function initAnimations() {
+  //hero
+  const heroTl = gsap.timeline({ delay: 0.5 });
+
+  heroTl
+    .fromTo(
+      '.top_section__right',
+      { yPercent: -100, opacity: 0 },
+      { yPercent: 0, opacity: 1, duration: 1, ease: 'power2.out' },
+    )
+    .fromTo(
+      '.figure',
+      { xPercent: 100, opacity: 0 },
+      { xPercent: 0, opacity: 1, duration: 1, ease: 'power2.out' },
+      '<',
+    );
+
+  //concept
+  gsap.fromTo(
+    '.concept-block',
+    { y: 50, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.concept-block',
+        start: 'top 95%',
+        end: 'bottom bottom',
+        toggleActions: 'play none none none',
+        fastScrollEnd: true,
+        once: true,
+      },
+    },
+  );
+
+  // advantages
   document.querySelectorAll('.advantages-item').forEach(block => {
     gsap.fromTo(
       block,
@@ -38,4 +79,74 @@ document.addEventListener('DOMContentLoaded', function() {
       },
     );
   });
-});
+
+  //summary decor animations
+  gsap.fromTo(
+    '.summary-decor-1, .summary-decor-2',
+    {
+      x: (index, target) => (target.classList.contains('summary-decor-1') ? -300 : 300),
+      opacity: 0,
+    },
+    {
+      x: 0,
+      opacity: 1,
+      duration: 1.2,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: '.summary-section',
+        start: 'top 60%',
+        fastScrollEnd: true,
+        once: true,
+      },
+    },
+  );
+
+  //summary image parallax
+  gsap.to('.summary-image ', {
+    y: -200,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: '.summary-content-block',
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: 1,
+    },
+  });
+
+  //gallery
+  gsap.fromTo(
+    '.gallery-images-block',
+    { y: 150, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.gallery-section',
+        start: 'top 50%',
+        toggleActions: 'play none none none',
+        fastScrollEnd: true,
+        once: true,
+      },
+    },
+  );
+
+  //about
+  gsap.fromTo(
+    '.about-decor-img-1, .about-decor-img-2',
+    { yPercent: 100, opacity: 0 },
+    {
+      duration: 1.5,
+      yPercent: 0,
+      opacity: 1,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: '.about-section',
+        start: 'top bottom',
+        end: 'bottom center',
+        toggleActions: 'play none none none',
+        once: true,
+        fastScrollEnd: true,
+      },
+    },
+  );
+}
